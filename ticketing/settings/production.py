36681f56cd03606a2e96c18defb876a3c5fdbcbf
Martin Atukunda <matlads@gmail.com>
2014-02-03 10:11:32 +0000

    Incorporate current production settings

--- ticketing/settings/production.py
+++ ticketing/settings/production.py
@@ -11,8 +11,19 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'ticketing',
         'USER': 'ticketing',
-        'PASSWORD':'',
+        'PASSWORD': '',
         'HOST': 'dbserver',
         'PORT': 6543,
     }
+}
+
+HELPDESK_VIEW_A_TICKET_PUBLIC = True
+HELPDESK_SUBMIT_A_TICKET_PUBLIC = False
+HELPDESK_DASHBOARD_BASIC_TICKET_STATS = True
+HELPDESK_FOOTER_SHOW_CHANGE_LANGUAGE_LINK = True
+HELPDESK_FOOTER_SHOW_API_LINK = True
+
+RAVEN_CONFIG = {
+    'dsn': 'http://496b2d50a2a8469294929e7775d56ea5:'
+           '0528086b2cb64e1a964973e5b2d80450@sentry.unicefuganda.org/5',
 }
