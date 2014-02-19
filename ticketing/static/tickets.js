3c33edfbca2d765d6294c614f82470f2df630975
Martin Atukunda <matlads@gmail.com>
2014-02-19 18:17:24 +0300

    Hide the Due on section of the add ticket page

---
+++ ticketing/static/tickets.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    // Hide the due On form-group
+    // note: It is still submitted, but as a blank set of fields.
+    $('#id_due_date_month').parent().parent().hide();
+})
